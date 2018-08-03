586d3ed8ba21999b7cf553a3ceabd3535299c8f8
gbbowers <gbbowers@gmail.com>
2018-08-03 15:46:12 -0500

    Some minor changes to include a category in frontmatter… this is where I was last time before some plugins borked stuff

---
+++ src/components/Bio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <p>bio</p>
+    )
+  }
+}
+
+export default Bio
---
+++ src/pages/guidelines/design-guidelines.md
@@ -0,0 +1,7 @@
+---
+title: Design Guidelines
+date: "2018-08-03"
+category: "guidelines"
+---
+
+# It worked!
